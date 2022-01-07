63e8315d01ba7d0595b1225ffba78e88e6f0fb77
megahcyberforce <97306662+megahcyberforce@users.noreply.github.com>
2022-01-08 03:31:23 +0800

    Add files via upload

---
+++ about.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+clear
+echo -e "================================================="
+echo -e "#         Premium Auto Script By DJASTV         #"
+echo -e "#-----------------------------------------------#"
+echo -e "# For Debian 9 & Debian 10 64 bit               #"
+echo -e "# For Ubuntu 18.04 & Ubuntu 20.04 64 bit        #"
+echo -e "# For VPS with KVM and VMWare virtualization    #"
+echo -e "# DJASTV PREMIUM                                #"
+echo -e "#-----------------------------------------------#"
+echo -e "# Thanks To                                     #"
+echo -e "#-----------------------------------------------#"
+echo -e "# ALLAH SWT                                     #"
+echo -e "# DjasTV                                        #"
+echo -e "# My Family                                     #"
+echo -e "# Google                                        #"
+echo -e "# LauranceLE                                    #"
+echo -e "# v2ray Indonesia                               #"
+echo -e "#                                               #"
+echo -e "#-----------------------------------------------#"
+echo -e "================================================="
